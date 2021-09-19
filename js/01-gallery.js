be8a64477c5495204393a4b3c51bf57a22795b98
Oleksandr S <oleksandr.slv@gmail.com>
2021-09-19 18:23:01 +0300

    adds markup for gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const markup = createMarkup(galleryItems);
+
+galleryRef.innerHTML = markup;
+
+
+function createMarkup(data) {
+  return data.map(item => {
+    return `<div class="gallery__item">
+              <a class="gallery__link" href="${item.original}">
+                <img
+                  class="gallery__image"
+                  src="${item.preview}"
+                  data-source="${item.original}"
+                  alt="${item.description}"
+                />
+              </a>
+            </div>`
+  }).join('');
+}
