78af297f714afc120127d534f161708338e1653c
Oleksandr S <oleksandr.slv@gmail.com>
2021-09-21 14:04:17 +0300

    adds close on esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,6 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryRef = document.querySelector('.gallery');
 const markup = createMarkup(galleryItems);
+let instance = {};
 
 galleryRef.innerHTML = markup;
 
@@ -12,16 +13,32 @@ galleryRef.addEventListener('click', (e) => {
   if (e.target.nodeName !== 'IMG') {
     return;
   }
-
+  
   openModalPicture(e.target);
 });
+  
+function createLightBox(picture) {
+  return basicLightbox.create(`
+          <img src="${picture.dataset.source}" width="800" height="600">
+  `);
+}
 
 function openModalPicture(picture) {
-  const instance = basicLightbox.create(`
-    <img src="${picture.dataset.source}" width="800" height="600">
-  `);
+  instance = createLightBox(picture);
 
   instance.show();
+  
+  window.addEventListener('keydown', closeModalOnEsc);
+}
+
+function closeModalOnEsc(e) {
+    if (e.code !== 'Escape') {
+      return;
+    }
+
+    instance.close();
+    
+    window.removeEventListener('keydown', closeModalOnEsc);
 }
 
 function createMarkup(data) {
